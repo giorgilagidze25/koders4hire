2a2563077f7017f40865c9ebb85c49a220b673d4
andu <lolandu575@gmail.com>
2026-01-24 16:24:03 +0400

    add link to user

--- src/components/ServicDetail..jsx
+++ src/components/ServicDetail..jsx
@@ -85,7 +85,9 @@ export default function ServiceDetail({ darkMode }) {
         />
         <div>
           <p className="font-semibold">{service.owner.real_name}</p>
-          <p className="text-sm text-gray-500">@{service.owner.username}</p>
+          <a href={`http://k4h.dev/${service.owner.username}`} className="text-sm text-gray-500">
+  @{service.owner.username}
+</a>
         </div>
       </div>
     )}
